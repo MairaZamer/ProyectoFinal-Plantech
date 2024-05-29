77ea587c07bde1bcc4a916c0909b69f1fc3aa187
GabTorres17 <g.abotorres@hotmail.com>
2024-05-29 18:30:34 -0400

    modelos definidos

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,9 @@
 const app = require('./src/app');
+const { conn } = require('./src/db')
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
+  conn.sync();
+  console.log(`Listening on port ${PORT}`);
+});
