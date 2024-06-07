1e893b228ec1f04138e266a88f6f7af9506c09b5
EdsonCasta <dedsoncasta07@gmail.com>
2024-06-06 21:36:26 -0500

    getByName

--- Backend/src/controllers/getTemplatesByName.js
+++ Backend/src/controllers/getTemplatesByName.js
@@ -1,28 +1,63 @@
-const { Products } = require('../db');
-const { Op } = require('sequelize');
-
+const axios = require('axios');
 
 async function templateByName(req, res) {
   try {
     const { name } = req.query;
 
-    const templates = await Products.findAll({
-      where: {
-        name: {
-          [Op.iLike]: `%${name}%`
-        }
-      }
-    });
+    if (!name) {
+      res.status(400).json({ error: 'El parámetro "name" es requerido' });
+      return;
+    }    
 
-    if (templates.length === 0) {
-      res.status(404).json({ error: 'No se encontro ningun resultado' })
+    const apiUrl = `https://my.api.mockaroo.com/prueba_pf.json?key=a5f575a0`;
+
+    const response = await axios.get(apiUrl);
+    const templates = response.data;
+
+    const filteredName = templates.filter(template => {
+      return template.name && template.name.toLowerCase().includes(name.toLowerCase());
+    });    
+
+    if (filteredName.length === 0) {
+      res.status(404).json({ error: 'No se encontró ningún resultado' });
       return;
     }
-    res.status(200).json(templates);
+    
+    res.status(200).json(filteredName);
 
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json({ error: error.message });
   }
 };
 
 module.exports = templateByName;
+
+
+// const { Products } = require('../db');
+// const { Op } = require('sequelize');
+
+
+// async function templateByName(req, res) {
+//   try {
+//     const { name } = req.query;
+
+//     const templates = await Products.findAll({
+//       where: {
+//         name: {
+//           [Op.iLike]: `%${name}%`
+//         }
+//       }
+//     });
+
+//     if (templates.length === 0) {
+//       res.status(404).json({ error: 'No se encontro ningun resultado' })
+//       return;
+//     }
+//     res.status(200).json(templates);
+
+//   } catch (error) {
+//     res.status(500).json(error.message);
+//   }
+// };
+
+// module.exports = templateByName;
