d3e5706fdd3a150344a30efc3148118080ad281b
GabTorres17 <g.abotorres@hotmail.com>
2024-06-11 15:32:51 -0400

    cambios index

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -3,16 +3,10 @@ const allTemplates = require('../controllers/getAllTemplates');
 const templatesById = require('../controllers/getTemplatesById');
 const templateByName = require('../controllers/getTemplatesByName');
 const { login } = require('../controllers/login');
-const createUser = require('../controllers/signUp');
 const pagination = require('../controllers/pagination');
-<<<<<<< HEAD
 const { filterByPrice, filterByTechnology } = require('../controllers/filters');
 const signUpController = require("../controllers/userController");
 const cors = require("cors");
-=======
-const { filterByPrice, filterByTechnology, filterByCategory } = require('../controllers/filters');
-const { sortTemplatesAlphabetically } = require('../controllers/filtersOrder');
->>>>>>> 752dce5b37c7ee81136b14730a1227dc1e216dd0
 
 const router = Router();
 router.use(cors());
@@ -24,12 +18,6 @@ router.post('/login', login);
 router.get('/pagination', pagination);
 router.get('/price', filterByPrice);
 router.get('/tech', filterByTechnology);
-<<<<<<< HEAD
 router.post('/register', signUpController.createUser)
-=======
-router.get('/category', filterByCategory);
-router.get('/alphabetical/:order', sortTemplatesAlphabetically);
-router.post('/signup', createUser);
->>>>>>> 752dce5b37c7ee81136b14730a1227dc1e216dd0
 
 module.exports = router;
