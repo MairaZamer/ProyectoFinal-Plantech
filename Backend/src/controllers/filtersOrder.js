fe7ce908e888a56af91d3abcc88df3d6a8e04d7a
EdsonCasta <dedsoncasta07@gmail.com>
2024-06-12 12:50:24 -0500

    filters and paginated

--- Backend/src/controllers/filtersOrder.js
+++ Backend/src/controllers/filtersOrder.js
@@ -4,7 +4,7 @@ const sortTemplatesAlphabetically = async (req, res) => {
     try {
         const { order } = req.params;
 
-        const response = await axios.get("https://my.api.mockaroo.com/prueba_pf.json?key=a5f575a0");
+        const response = await axios.get("https://my.api.mockaroo.com/e_books_palace.json?key=a5f575a0");
         const templates = response.data;
         
         const sortedTemplates = templates.sort((a, b) => {
@@ -20,4 +20,29 @@ const sortTemplatesAlphabetically = async (req, res) => {
     }
 };
 
-module.exports = { sortTemplatesAlphabetically };
+const filterByPrice = async (req, res) => {
+    try {
+        const { sort } = req.query;
+
+        const response = await axios.get("https://my.api.mockaroo.com/e_books_palace.json?key=a5f575a0");
+        let books = response.data;
+
+        if (sort === 'asc') {
+            books.sort((a, b) => a.price - b.price);
+        } else if (sort === 'desc') {
+            books.sort((a, b) => b.price - a.price);
+        }
+
+        if (books.length === 0) {
+            return res.status(404).json({ message: "No se encontraron productos que coincidan con el filtro." });
+        }
+
+        res.status(200).json(books);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+};
+
+module.exports = { sortTemplatesAlphabetically, filterByPrice };
